7f620be58348c81027c607804d239fb09232c794
Brett Wise <brettdwise@gmail.com>
2018-07-10 18:05:38 -0400

    Fixed config/compile warning

--- config/test.exs
+++ config/test.exs
@@ -1,3 +1,4 @@
 use Mix.Config
 
+Code.require_file "../test/support/in_memory_http_client.ex", __DIR__
 config :decipher_api, service: DecipherAPITest.Support.InMemoryHTTPClient
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,3 @@
 Code.require_file "support/fake_data.ex", __DIR__
-Code.require_file "support/in_memory_http_client.ex", __DIR__
 DecipherAPITest.Support.InMemoryHTTPClient.start()
 ExUnit.start()
