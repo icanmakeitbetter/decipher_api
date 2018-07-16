49f869d344cd6ffa2a294056b4c9b71a47df5c9a
Brett Wise <brettdwise@gmail.com>
2018-07-16 16:52:14 -0400

    Altered value module and tests for easier lookup

--- lib/decipher_api/datamap/value.ex
+++ lib/decipher_api/datamap/value.ex
@@ -1,32 +1,13 @@
 defmodule DecipherAPI.Datamap.Value do
-  alias __MODULE__
-
-  defstruct(
-    title: "",
-    value: nil
-  )
-
-  @spec new() :: %Value{}
-  def new() do
-    %Value{}
-  end
 
   def check_maybe_coerce(nil) do
     nil
   end
 
-  @spec check_maybe_coerce([%{}]) :: [%Value{}]
+  @spec check_maybe_coerce([%{}]) :: [%{}]
   def check_maybe_coerce(values) when is_list(values) do
     values
-    |> Enum.map(&coerce_data/1)
-    |> Enum.into(Map.new(), fn v -> {v.value, v.title} end)
+    |> Enum.into(Map.new(), fn v -> {v["value"], v["title"]} end)
   end
 
-  @spec coerce_data(%{}) :: %Value{}
-  def coerce_data(value_pair) when is_map(value_pair) do
-    %{
-      title: value_pair["title"],
-      value: value_pair["value"]
-    }
-  end
 end
--- test/datamap/value_test.exs
+++ test/datamap/value_test.exs
@@ -2,30 +2,19 @@ defmodule DecipherAPITest.DatamapTest.ValueTest do
   alias DecipherAPI.Datamap.Value
   use ExUnit.Case, async: true
 
-  test "that Value.new/0 creates a struct" do
-    new_struct = %Value{title: "", value: nil}
-    assert Value.new() == new_struct
-  end
-
   test "that if nil is passed it returns nil" do
     assert Value.check_maybe_coerce(nil) == nil
   end
 
-  test "that if a list of maps is passed it returns a list of struct/s" do
+  test "that if a list of maps is passed it returns a keyed map" do
     values = [
-      %{"title" => "Example1", "value" => 3},
-      %{"title" => "Example2", "value" => 4}
+      %{"title" => "Example1", "value" => 1},
+      %{"title" => "Example2", "value" => 2},
+      %{"title" => "Example3", "value" => 3},
+      %{"title" => "Example4", "value" => 4}
     ]
 
-    assert Value.check_maybe_coerce(values) == [
-      %Value{title: "Example1", value: 3},
-      %Value{title: "Example2", value: 4}
-    ]
+    assert Value.check_maybe_coerce(values) == %{1 => "Example1", 2 => "Example2", 3 => "Example3", 4 => "Example4"}
   end
 
-  test "that passing a map returns a struct" do
-    map = %{"title" => "Example2", "value" => 4}
-
-    assert Value.coerce_data(map) == %Value{title: "Example2", value: 4}
-  end
 end
