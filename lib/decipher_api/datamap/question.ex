8b1fa60d5ddf54335d5f8c0d24fc18eb4fd1059e
Brett Wise <brettdwise@gmail.com>
2018-07-20 17:19:30 -0400

    changed default struct values to nil. added cond to add question types to struct

--- lib/decipher_api/datamap/question.ex
+++ lib/decipher_api/datamap/question.ex
@@ -3,12 +3,13 @@ defmodule DecipherAPI.Datamap.Question do
   alias DecipherAPI.Datamap.{Variables, Value}
 
   defstruct(
-    grouping: "",
-    qlabel: "",
-    qtitle: "",
-    type: "",
-    values: [],
-    variables: []
+    grouping: nil,
+    qlabel: nil,
+    qtitle: nil,
+    type: nil,
+    __ui_type__: nil,
+    values: nil,
+    variables: nil
   )
 
   @spec new() :: %Question{}
@@ -33,7 +34,89 @@ defmodule DecipherAPI.Datamap.Question do
   def coerce_maps(questions) when is_map(questions) or is_list(questions) do
     questions
     |> Enum.map(&new/1)
-    |> Enum.into(Map.new, fn q -> {q.qlabel, q} end)
+    |> Enum.into(Map.new, fn(q) ->
+
+         type      = q.type
+         grouping  = q.grouping
+         variables = q.variables
+
+         ui_type =
+           cond do
+             single_select?(type, grouping) ->
+               :single_select
+             single_select_matrix?(type, grouping, variables) ->
+               :single_select_matrix
+             multi_select?(type, grouping) ->
+               :multi_select
+             multi_select_matrix?(type, grouping) ->
+               :multi_select_matrix
+             simple_number?(type, grouping) ->
+               :number
+             autosum?(type, grouping) ->
+               :autosum
+             float?(type) ->
+               :float
+             text?(type) ->
+               :text
+             true ->
+               :unknown_type
+           end
+
+         {q.qlabel, %{q | __ui_type__: ui_type}}
+
+       end)
+  end
+
+  def single_select?(type, grouping) do
+    single?(type) && cols?(grouping)
+  end
+
+  def single_select_matrix?(type, grouping, variables) do
+    single?(type) && rows?(grouping) && Enum.count(variables) > 1
+  end
+
+  def multi_select?(type, grouping) do
+    multiple?(type) && cols?(grouping)
+  end
+
+  def multi_select_matrix?(type, grouping) do
+    multiple?(type) && rows?(grouping)
+  end
+
+  def simple_number?(type, grouping) do
+    number?(type) && rows?(grouping)
+  end
+
+  def autosum?(type, grouping) do
+    number?(type) && cols?(grouping)
+  end
+
+  def cols?(grouping) do
+    grouping == "cols"
+  end
+
+  def rows?(grouping) do
+    grouping == "rows"
+  end
+
+  def single?(type) do
+    type == "single"
+  end
+
+  def text?(type) do
+    type == "text"
+  end
+
+  def multiple?(type) do
+    type == "multiple"
+  end
+
+  def number?(type) do
+    type == "number"
+  end
+
+  def float?(type) do
+    type == "float"
   end
 
 end
