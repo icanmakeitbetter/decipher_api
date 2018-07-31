a3f472de7680071fb6e1e2125c68972356a2bfc6
Brett Wise <brettdwise@gmail.com>
2018-07-31 17:31:48 -0400

    Now parsing xml to get proper order and page breaks

--- lib/decipher_api/datamap.ex
+++ lib/decipher_api/datamap.ex
@@ -40,7 +40,6 @@ defmodule DecipherAPI.Datamap do
       do
         %{
           datamap |
-          question_order: Enum.map(datamap_metadata["questions"], fn question -> question["qlabel"] end),
           questions: datamap_metadata["questions"],
           variables: datamap_metadata["variables"],
           xml: xml_metadata
@@ -58,6 +57,7 @@ defmodule DecipherAPI.Datamap do
     %{
       datamap |
       survey_name: xml_metadata.name,
+      question_order: xml_metadata.ordering,
       questions: Question.coerce_maps(datamap.questions, xml_metadata.questions),
       variables: Variables.coerce_maps(datamap.variables),
       xml: xml_metadata
@@ -67,19 +67,50 @@ defmodule DecipherAPI.Datamap do
   @spec coerce_xml_metadata(String.t) :: %{}
   def coerce_xml_metadata(xml_metadata) when is_binary(xml_metadata) do
 
-    questions =
+    ordering =
       xml_metadata
-      |> xpath(~x"//*[@label]"el)
-      |> Enum.map(fn node ->
-        xpath(node, ~x"@*"el)
-        |> Enum.map(fn{:xmlAttribute, name, _, _, _, _, _, _, value, _} -> {name, to_string(value)} end)
-        |> Kernel.++([comment: xpath(node, ~x"comment/text()") |> to_string]) end)
-        |> Enum.into(%{}, fn(metadata) ->
-          {Keyword.get(metadata, :label), Map.new(metadata)}
-        end)
+      |> xpath(~x"/survey/*"el)
+      |> Enum.filter(fn
+        {:xmlElement, :suspend, _, _, _, _, _, _, _, _, _, _} ->
+          true
+        {:xmlElement, _, _, _, _, _, _, attrs, _, _, _, _} ->
+          Enum.any?(attrs, fn
+            {:xmlAttribute, :label, _, _, _, _, _, _, _, _} ->
+              true
+            _attr ->
+              false
+          end)
+        _node ->
+          false
+      end)
+      |> Enum.map(fn
+        {:xmlElement, :suspend, _, _, _, _, _, _, _, _, _, _} ->
+          :page_break
+        {:xmlElement, _, _, _, _, _, _, attrs, _, _, _, _} = node ->
+          attrs
+          |> Enum.into(Map.new, fn {:xmlAttribute, name, _, _, _, _, _, _, value, _} ->
+            {name, to_string(value)}
+          end)
+          |> Map.put(:comment, node |> xpath(~x"comment/text()"s))
+      end)
+      |> Enum.chunk_by(fn question_or_break -> question_or_break == :page_break end)
+      |> Enum.reject(fn
+        [:page_break] ->
+          true
+        _page ->
+          false
+      end)
+
+    questions =
+      ordering
+      |> List.flatten()
+      |> Enum.into(%{}, fn(metadata) ->
+        {Map.get(metadata, :label), metadata}
+      end)
 
       %{
         name: xpath(xml_metadata, ~x"@alt") |> to_string,
+        ordering: Enum.map(ordering, fn page -> Enum.map(page, fn question -> question.label end) end),
         questions: questions
       }
   end
--- test/datamap_test.exs
+++ test/datamap_test.exs
@@ -3,13 +3,13 @@ defmodule DecipherAPITest.DatamapTest do
   alias DecipherAPI.Datamap
 
   use ExUnit.Case, async: true
-  @survey_url FakeData.survey_url
+  @survey_url FakeData.survey_url()
   @datamap %Datamap{survey_id: @survey_url}
 
   test "calling build_datamap_set fetches and coerces the data" do
-    assert @datamap.questions == Map.new
+    assert @datamap.questions == Map.new()
     coerced = Datamap.build_metadata_set(@datamap)
-    refute coerced.questions == Map.new
+    refute coerced.questions == Map.new()
   end
 
   test "that calling Datamap.new() returns a new struct in the correct format" do
@@ -17,31 +17,38 @@ defmodule DecipherAPITest.DatamapTest do
   end
 
   test "that coerce_data actually does that" do
-    datamap = FakeData.raw_all_question_datamap_response
+    datamap = FakeData.raw_all_question_datamap_response()
     assert is_list(datamap.questions)
+
     assert Enum.all?(datamap.questions, fn q ->
-      is_map(q) and not Map.has_key?(q, :__struct__)
-    end)
+             is_map(q) and not Map.has_key?(q, :__struct__)
+           end)
+
     assert is_binary(datamap.xml)
 
     coerced = Datamap.coerce_data(datamap)
     assert is_map(coerced.questions)
+
     assert Enum.all?(coerced.questions, fn
-      {label, %Datamap.Question{qlabel: label}} ->
-        true
-      _kv ->
-        false
-    end)
+             {label, %Datamap.Question{qlabel: label}} ->
+               true
+
+             _kv ->
+               false
+           end)
+
     assert is_map(coerced.xml)
   end
 
   test "test that the question_order field gives us an ordered list" do
-    assert Datamap.get_question_metadata(%Datamap{survey_id: "something/555/something"}).question_order() ==
-      [
-       "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10",
-       "q13", "q14", "q15", "q33", "q34", "q16", "q20", "q22", "q23", "q24", "q25",
-       "q25_norm", "q28", "q29", "q31", "q30", "q21", "q32", "q35", "q36", "q37",
-       "q39", "noanswer", "qtime", "start_date"
-      ]
+    assert Datamap.build_metadata_set(%Datamap{survey_id: "something/555/something"}).question_order() ==
+     [
+       [
+         "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "cm1",
+         "q10", "q27", "q13", "q14", "q15", "q33", "q34", "q16", "q20",
+         "q22", "q23", "q24", "q25", "q28", "q29", "q31", "q30", "q21",
+         "q32", "q35", "q36", "q37", "q38", "q39", "q40", "q41"
+       ]
+     ]
   end
 end
